fa41e828702e2494c8d78ab6dfaad39ed80fe6c5
colbisaurusrex <c.barthelmess1@gmail.com>
2017-04-28 12:52:38 -0400

    log api key to test

--- client/landing/landingController.js
+++ client/landing/landingController.js
@@ -4,6 +4,7 @@ angular.module('landing', ['shareState'])
   .controller('landingController', function ($scope, $http, $location, storePhotos) {
     $scope.pingFlickr = () => {
       const flickrKey = process.env.FLICKR_API_KEY
+      console.log(flickrKey)
       return $http.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${flickrKey}=&photoset_id=72157634968390409&format=json&nojsoncallback=1`)
     .then(({ data }) => {
       console.log(data)
