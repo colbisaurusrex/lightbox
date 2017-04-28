25061ac5f0321376b9f9deb0509234da3ca4c83b
colbisaurusrex <c.barthelmess1@gmail.com>
2017-04-28 02:17:08 -0400

    configure webpack, add package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   context: __dirname + '/client',
-  //may need to change this
-  entry: './app.js',
+  // may need to change this
+  entry: './js/app.js',
   output: {
-    path: __dirname+ '/client',
-    filename: 'bundle.js'
-  }
-}
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
